74e4f82156408dd8db26e681ca9adcc4f2455f3a
Jorge Parra <jorge.parra@karibu.cl>
2026-08-18 21:16:12 -0400

    Callback, promises y async-await

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, NotFoundException, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Res } from '@nestjs/common';
 import type { Response } from 'express';
 
 @Controller()
 export class AppController {
+
+  LISTA = [
+    { id: 1, rut: '123', nombre: 'Jorge' },
+    { id: 2, rut: '456', nombre: 'Ana' },
+  ];
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(
-    @Res() res: Response
-  ) {
-    const salida = this.appService.getHello();
-    console.log(1);
-    
+  @Get('callback/:id')
+  getUsuarioCallback(@Param('id') id: number,  @Res() res: Response) {
+    /*
     setTimeout(() => {
       console.log(2);
       setTimeout(() => {
@@ -24,9 +26,116 @@ export class AppController {
         }, 5000);
       }, 1000);
     }, 2000);
-
-    console.log(4);
-    
-    // return this.appService.getHello();
+    */
+    console.log(1);
+    this.obtenerUsuarioCallback(id, (error, usuario) => {
+      if (error) {
+        res.status(404).send(error.message);
+      } else {
+        res.status(201).send(usuario);
+      }
+    });
+    console.log(2);
   }
+
+  obtenerUsuarioCallback(id: number, retornarUsuario) {
+    const usuario = this.LISTA.find((usuario) => usuario.id == id);
+    console.log('Usuario encontrado:', usuario);
+    if (!usuario) {
+      retornarUsuario(new Error('Usuario no encontrado'));
+    } else {
+      retornarUsuario(null, usuario);
+    }
+  }
+
+  @Get('promise/:id/:id2')
+  getUsuarioPromise(@Param('id') id: number, @Param('id2') id2: number,  @Res() res: Response) {
+    const promesa = this.obtenerUsuarioPromesa(id);
+    const promesa2 = this.obtenerUsuarioPromesa(id2);
+    
+    promesa.then((usuario) => {
+      promesa2.then((usuario2) => {
+        console.log("Promesa 2 resuelta")
+        res.status(200).send({usuario1: usuario, usuario2: usuario2});
+      }).catch((error) => {
+        console.log("Promesa 2 rechazada")
+        res.status(200).send({usuario1: usuario, mensaje2: error.message});
+      });
+    })
+    .catch((error1) => {
+      console.log("Promesa 1 rechazada")
+      promesa2.then((usuario2) => {
+        console.log("Promesa 2 resuelta")
+        res.status(200).send({mensaje1: error1.message, usuario2});
+      }).catch((error2) => {
+        console.log("Promesa 2 rechazada")
+        res.status(404).send({mensaje1: error1.message, mensaje2: error2.message});
+      });
+    });
+    
+  }
+
+  @Get('promise-encadenada/:id/:id2')
+  getUsuarioPromiseEncadenada(@Param('id') id: number, @Param('id2') id2: number,  @Res() res: Response) {
+    const promesa = this.obtenerUsuarioPromesa(id);
+    
+    promesa.then((usuario) => {
+      console.log("Promesa 1 resuelta")
+      return this.obtenerUsuarioPromesa(id2);
+    }).then(usuario2 => {
+      console.log("Promesa 2 resuelta")
+      res.status(200).send({ usuario2: usuario2});
+    }).catch((error) => {
+      console.log("Promesa 1 o 2 rechazada")
+      res.status(404).send({mensaje: error.message});
+    });
+  }
+
+  obtenerUsuarioPromesa(id: number) {
+    console.log("Promesa iniciada: ", id);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const usuario = this.LISTA.find((usuario) => usuario.id == id);
+        console.log('Usuario:', usuario);
+        if (!usuario) {
+          reject(new Error('Usuario no encontrado'));
+        } else {
+          resolve(usuario);
+        }
+      }, 3000);
+    })
+  }
+
+  @Get('async-then')
+  async getUsuarioAsyncThen(@Res() res: Response) {
+    const promesa = this.ejemplo();
+    promesa.then((resultado) => {
+      console.log("Promesa resuelta")
+      res.status(200).send({resultado});
+    }).catch((error) => {
+      console.log("Promesa rechazada")
+      res.status(404).send({mensaje: error.message});
+    });
+  }
+
+  async ejemplo() : Promise<string> {
+    return 'ok';
+  }
+
+  @Get('async-await/:id/:id2/:id3')
+  getUsuarioAsyncAwait(@Param('id') id: number, @Param('id2') id2: number, @Param('id3') id3: number) {
+    console.log(new Date().toISOString(), "Inicio de la ejecución");
+    try {
+      return this.appService.obtenerListaUsuarios(id, id2, id3);
+    } catch (error) {
+      throw new HttpException(error.message, 404);
+      // throw new NotFoundException(error.message);
+    }
+  }
+
+
+
+
+
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,37 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Jorge';
+
+  LISTA = [
+    { id: 1, rut: '123', nombre: 'Jorge' },
+    { id: 2, rut: '456', nombre: 'Ana' },
+  ];
+
+  async obtenerListaUsuarios(id1, id2, id3): Promise<any[]> {
+    const promise1 = this.obtenerUsuarioAsync(id1, 1);
+    const promise2 = this.obtenerUsuarioAsync(id2, 3);
+    const promise3 = this.obtenerUsuarioAsync(id3, 5);
+    const usuario1 = await promise1;
+    const usuario2 = await promise2;
+    const usuario3 = await promise3;
+    return [ usuario1, usuario2, usuario3 ];
   }
+
+  async obtenerUsuarioAsync(id: number, segundos: number): Promise<any> {
+    const usuario : any = this.LISTA.find((usuario) => usuario.id == id);
+    await this.esperar(segundos);
+    if (!usuario) {
+      throw new Error('Usuario no encontrado');
+    }
+    return usuario;
+  }
+
+  esperar(segundos: number): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, segundos * 1000);
+    });
+  }
+
 }
